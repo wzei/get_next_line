a07ddba8aa4bcd71c87f049f8bf2891c42c638d9
Wisp Zei <wzei@e3r2p5.21-school.ru>
2019-01-11 20:04:30 +0300

    Add buff_size

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 05:40:27 by wzei              #+#    #+#             */
-/*   Updated: 2018/12/23 05:41:30 by wzei             ###   ########.fr       */
+/*   Updated: 2018/12/23 05:51:07 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+
+# define BUFF_SIZE 32
 
 int	get_next_line(const int fd, char **line);
 
