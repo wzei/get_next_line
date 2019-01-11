e13683294ef14608cb62b43a27a96ed4d2287f89
Wisp Zei <wzei@e3r2p5.21-school.ru>
2019-01-11 20:56:19 +0300

    add GNL header

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,11 @@
 /*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 05:39:38 by wzei              #+#    #+#             */
-/*   Updated: 2018/12/23 05:40:09 by wzei             ###   ########.fr       */
+/*   Updated: 2019/01/11 20:55:59 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
 
 int	get_next_line(const int fd, char **line)
 {
